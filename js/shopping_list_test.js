523c11529b413e2a8f1624ae2383dc2c33696396
Oksana Harr <oksana.harris@gmail.com>
2017-07-05 18:58:58 -1000

    one test written

---
+++ js/shopping_list_test.js
@@ -0,0 +1,15 @@
+/* jshint esversion: 6 */
+
+// const Person = require('../app.js');
+
+var expect = chai.expect;
+var should = chai.should();
+
+
+describe('ShoppingListItem', function (){
+
+  it('should be a class (function) ;) ', function (){
+    expect(ShoppingListItem).to.be.a('function');
+  });
+
+});

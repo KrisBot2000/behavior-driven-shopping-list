ab3697d57106e6095b66a4f90cb64a704acf3fbc
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-07-05 19:12:39 -1000

    add name property test

--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -8,8 +8,14 @@ var should = chai.should();
 
 describe('ShoppingListItem', function (){
 
+  var newItem = new ShoppingListItem('pencil');
+
   it('should be a class (function) ;) ', function (){
     expect(ShoppingListItem).to.be.a('function');
+  });
+
+  it('should have a name property', function(){
+    expect(newItem.name).to.equal('pencil');
   });
 
 });

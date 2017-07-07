1c7e6ba81d24ec145f81195c227f1a311d67bc0d
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-07-06 15:13:17 -1000

    newest

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -13,4 +13,25 @@ class ShoppingList {
       this.items.unshift(singleItem);
     }
   }
+
+  removeItem(singleItem){
+    if(singleItem instanceOf ShoppingListItem){
+      if(this.items.length>0){
+        if(singleItem===undefined){
+          this.item.pop();
+        }
+        if(this.items.indexOf(singleItem) > -1){
+          this.items.splice((this.items.indexOf(singleItem)),1);
+        }
+      }else{
+        //do nothing.
+      }
+    }else{
+      throw "Error: item is not a ShoppingListItem";
+    }
+  }
+
+
+
 }
+
